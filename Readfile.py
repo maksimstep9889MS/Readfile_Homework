19eb96acbb80f15e2898032f6a9389fbfac9a6ed
MaksimStep <maksimstep9889@gmail.com>
2025-02-14 21:32:00 +0300

    Задание 2

--- Readfile.py
+++ Readfile.py
@@ -37,17 +37,37 @@ def read_recipes(textfile):
 
     return cook_book
 
-def print_cook_book(cook_book):
-    for dish, ingredients in cook_book.items():
-        print(f"  '{dish}': [")
-        ingredient_strings = [
-            f"    {{'ingredient_name': '{ing['ingredient_name']}', 'quantity': {ing['quantity']}, 'measure': '{ing['measure']}'}}"
-            for ing in ingredients
-        ]
-        print(',\n'.join(ingredient_strings))
-        print(" ],")
+def get_shop_list_by_dishes(dishes, person_count, cook_book):
+    shop_list = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredient in cook_book[dish]:
+                ingredient_name = ingredient['ingredient_name']
+                quantity = ingredient['quantity'] * person_count
+                measure = ingredient['measure']
 
-textfile= 'recipes.txt'
+                if ingredient_name not in shop_list:
+                    shop_list[ingredient_name] = {'measure': measure, 'quantity': quantity}
+                else:
+                    shop_list[ingredient_name]['quantity'] += quantity
+        else:
+            print(f"Блюдо '{dish}' не найдено в кулинарной книге.")
+    return shop_list
+
+def print_formatted_shop_list(shop_list):
+    print("{")
+    for ingredient, details in shop_list.items():
+        print(f"  '{ingredient}': {details},")  
+    print("}")
+
+
+textfile = 'recipes.txt'
 cook_book = read_recipes(textfile)
 
-print_cook_book(cook_book)
+
+if cook_book:  
+    dishes = ['Запеченный картофель', 'Омлет']  
+    person_count = 2  
+
+    shopping_list = get_shop_list_by_dishes(dishes, person_count, cook_book)  
+    print_formatted_shop_list(shopping_list) 
